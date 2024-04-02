14435affaa9276065dae4a2b135778111265fe8a
yukio87 <Andrey73ulsk@gmail.com>
2024-04-02 16:20:30 +0400

    F-4_fix#3

--- src/widgets/carpage-main/ui/CarPageMain.jsx
+++ src/widgets/carpage-main/ui/CarPageMain.jsx
@@ -1,11 +1,28 @@
+/* eslint-disable react/no-unstable-nested-components */
+// отключил это правило только временно
 import { SelectCar } from '@features/select-car'
-import { ErrorFallback } from '@shared/ui/error-fallback'
+// import { ErrorFallback } from '@shared/ui/error-fallback'
+import { QueryErrorResetBoundary } from '@tanstack/react-query'
 import { ErrorBoundary } from 'react-error-boundary'
 
 export function CarPageMain() {
   return (
-    <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <SelectCar />
-    </ErrorBoundary>
+    <QueryErrorResetBoundary>
+      {({ reset }) => (
+        <ErrorBoundary
+          onReset={reset}
+          fallbackRender={({ resetErrorBoundary }) => (
+            <div>
+              There was an error!
+              <button onClick={() => resetErrorBoundary()} type="button">
+                Try again
+              </button>
+            </div>
+          )}
+        >
+          <SelectCar />
+        </ErrorBoundary>
+      )}
+    </QueryErrorResetBoundary>
   )
 }
