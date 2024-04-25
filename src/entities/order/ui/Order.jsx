3e11a07cbf8a019d5652e5b97445d939aa561f33
yukio87 <Andrey73ulsk@gmail.com>
2024-04-25 16:28:57 +0400

    V-4,F-5_fix#2

--- src/entities/order/ui/Order.jsx
+++ src/entities/order/ui/Order.jsx
@@ -1,21 +1,18 @@
 import { routesPaths } from '@shared/consts/routesPaths'
-import { getNumberWithSpaces } from '@shared/lib/format'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 import { options } from '../consts/options'
-import { getPriceRangeString } from '../lib/format'
 import { useNavigateTo } from '../lib/hooks/useNavigateTo'
 import { usePageDataIsFilled } from '../lib/hooks/usePageDataIsFilled'
-import { getOrderPost } from '../model/orderPostSlice'
 import { getOrderUi } from '../model/orderUiSlice'
 import { OrderDetail } from './components'
-import { orderContainer, orderStyles, priceStyles } from './Order.module.scss'
+import { Price } from './components/Price/Price'
+import { orderContainer, orderStyles } from './Order.module.scss'
 
 export function Order() {
   const navigate = useNavigate()
   const orderUi = useSelector(getOrderUi)
-  const { carId } = useSelector(getOrderPost)
 
   const curPageDataIsFilled = usePageDataIsFilled()
   const { nextPathName, buttonText } = useNavigateTo()
@@ -36,14 +33,7 @@ export function Order() {
             ),
         )}
       </div>
-      {Object.keys(carId).length > 0 && (
-        <p className={priceStyles}>
-          Цена:{' '}
-          {getNumberWithSpaces(orderUi.price) ||
-            getPriceRangeString(carId.priceMin, carId.priceMax)}{' '}
-          ₽
-        </p>
-      )}
+      <Price />
       <button
         disabled={
           nextPathName === pathLocationPage ? false : !curPageDataIsFilled
